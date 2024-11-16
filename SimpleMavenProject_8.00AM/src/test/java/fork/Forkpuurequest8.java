fa66dbddb443f56b8c279743b23af886af48db7a
Gamya <Gamya@LAPTOP-ERD397M4>
2024-11-16 09:33:07 +0530

    forkCommit

---
+++ SimpleMavenProject_8.00AM/src/test/java/fork/Forkpuurequest8.java
@@ -0,0 +1,13 @@
+package fork;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Forkpuurequest8 {
+	@Test
+	public void forkandpull() {
+		Reporter.log("added fork and pull request");
+		
+	}
+
+}
