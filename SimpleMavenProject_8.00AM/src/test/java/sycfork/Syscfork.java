2e661f6b445efd4b6e1c37192c405fc531a064c7
Administrator <Administrator@DESKTOP-1ULGF16>
2024-11-18 09:16:12 +0530

    syncfork

---
+++ SimpleMavenProject_8.00AM/src/test/java/sycfork/Syscfork.java
@@ -0,0 +1,11 @@
+package sycfork;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Syscfork {
+@Test
+public void syncfork() {
+	Reporter.log("added syncfork");
+}
+}
