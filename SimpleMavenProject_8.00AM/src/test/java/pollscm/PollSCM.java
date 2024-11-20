a74c00e150d6c72df0b76e229072824f042d5c18
Administrator <Administrator@DESKTOP-1ULGF16>
2024-11-20 09:30:22 +0530

    pollscm is added

---
+++ SimpleMavenProject_8.00AM/src/test/java/pollscm/PollSCM.java
@@ -0,0 +1,11 @@
+package pollscm;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PollSCM {
+@Test
+public void pscm() {
+	Reporter.log("pollscm is added");
+}
+}
